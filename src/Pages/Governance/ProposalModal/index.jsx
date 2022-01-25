683dd82837e97a1ff75a649619d71c0ae8510e41
styled <smyvensestime@gmail.com>
2022-01-24 22:21:43 -0500

    working proposals

--- src/Pages/Governance/ProposalModal/index.jsx
+++ src/Pages/Governance/ProposalModal/index.jsx
@@ -1,10 +1,37 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import { useState } from 'react'
 
-import {Modal, Typography} from "@mui/material";
-import {ButtonGroup, GoldenBoldText, StyledBox, TextSection, ProposalInput} from "./styles";
-import DarkButton from "../../../Components/Styles/DarkButton";
+import { Modal, Typography } from '@mui/material'
+import { ButtonGroup, GoldenBoldText, StyledBox, TextSection, ProposalInput } from './styles'
+import DarkButton from '../../../Components/Styles/DarkButton'
+import proposalsSample from '../proposals'
 
-function ModalComponent({open, handleClose, data, type}) {
+const ModalComponent = ({ open, handleClose, data, type }) => {
+    const [proposalName, setProposalName] = useState('')
+    const [proposalText, setProposalText] = useState('')
+
+    /**
+     * add new proposal to proposal to database
+     */
+    const newProposal = () => {
+        const date = new Date('2010-10-11T00:00:00+05:30')
+        const today =
+            (date.getMonth() > 8 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1)) +
+            '/' +
+            (date.getDate() > 9 ? date.getDate() : '0' + date.getDate()) +
+            '/' +
+            date.getFullYear()
+
+        const nextYear = (parseInt(new Date().getFullYear()))
+
+        proposalsSample.push({
+            name: proposalName,
+            text: proposalText,
+            status: 'active',
+            start_date: today,
+            end_date: today.substring(today) + (nextYear + 1).toString(),
+        })
+    }
 
     return (
         <Modal
@@ -14,29 +41,29 @@ function ModalComponent({open, handleClose, data, type}) {
             onClose={handleClose}
         >
             <StyledBox>
-                {type === "new" ?
+                {type === 'new' ? (
                     <>
                         <GoldenBoldText>New proposal</GoldenBoldText>
-                        <ProposalInput type="text" size="40"/>
+                        <ProposalInput type="text" size="40" onChange={e => setProposalName(e.target.value)} />
+                        <ProposalInput type="text" size="70" onChange={e => setProposalText(e.target.value)} />
                         <TextSection>What is your proposal?</TextSection>
-                        <DarkButton>Send</DarkButton>
-                    </> :
+                        <DarkButton onClick={() => newProposal()}>Send</DarkButton>
+                    </>
+                ) : (
                     <>
                         <GoldenBoldText id="modal-modal-title" variant="h6" component="h2">
                             {data.name}
                         </GoldenBoldText>
-                        <TextSection>
-                            {data.text}
-                        </TextSection>
+                        <TextSection>{data.text}</TextSection>
                         <ButtonGroup>
                             <DarkButton>Approve</DarkButton>
                             <DarkButton>Reject</DarkButton>
                         </ButtonGroup>
                     </>
-                }
+                )}
             </StyledBox>
         </Modal>
-    );
+    )
 }
 
-export default ModalComponent;
+export default ModalComponent
--- src/Pages/Governance/index.jsx
+++ src/Pages/Governance/index.jsx
@@ -16,30 +16,9 @@ import {
 import DarkButton from '../../Components/Styles/DarkButton'
 import { useEffect, useState } from 'react'
 import ModalComponent from './ProposalModal'
+import proposalsSample from '../Governance/proposals'
 
-const proposalsSample = [
-    {
-        name: 'Proposal 2',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto commodi consequuntur cupiditate error ex id, iure laudantium magnam natus, placeat porro reiciendis rem tempora tempore temporibus vitae. Eligendi, molestias?',
-        status: 'active',
-        start_date: '05/11/2021',
-        end_date: '20/11/2021',
-    },
-    {
-        name: 'Proposal 1',
-        text: 'Give lambo to top 5 players',
-        status: 'active',
-        start_date: '03/11/2021',
-        end_date: '10/11/2021',
-    },
-    {
-        name: 'Proposal 0',
-        text: 'Make more dolls',
-        status: 'closed',
-        start_date: '03/10/2021',
-        end_date: '10/10/2021',
-    },
-]
+
 
 function Governance() {
     const [active, setActive] = useState('all')
