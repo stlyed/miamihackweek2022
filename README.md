c069093b504650fa77d196f169f6f98c76ae58b3
styled <smyvensestime@gmail.com>
2022-01-24 20:29:21 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MiamiHackWeek2022
+
+install the dependices: `npm i`
+
+start a webserver in 3000 `npm start`
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-function App() {
+const App = () => {
     return (
         <div className="App">
             <h1>Hello World!</h1>
